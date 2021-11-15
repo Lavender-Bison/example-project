245c2994dcbf83cb7291edb77b4c5cf0fc877f00
Piper Dougherty <doughertypiper@gmail.com>
2021-11-15 13:40:10 -0600

    No s.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -32,7 +32,7 @@ resource "google_compute_instance_template" "webserver" {
   }
 
   network_interface {
-    subnetworks = "projects/network-prod-2a8575/regions/us-central1/subnetworks/general-purpose-computation-dev"
+    subnetwork = "projects/network-prod-2a8575/regions/us-central1/subnetworks/general-purpose-computation-dev"
   }
 
   metadata = {
